fe909e9c687ecf5fc3c2b89b44c14c5a342ca5d1
Bogdan Popa <bogdan@defn.io>
2020-05-17 21:08:36 +0300

    core: clean up make-requester

--- http-easy/http-easy.rkt
+++ http-easy/http-easy.rkt
@@ -17,27 +17,16 @@
 
  make-timeout-config
 
- (all-from-out "http-easy/private/error.rkt")
  (all-from-out "http-easy/private/response.rkt")
  (all-from-out "http-easy/private/session.rkt"))
 
-(define/contract ((make-requester method) url-or-string
-                                          #:headers [headers (hasheq)]
-                                          #:params [params null]
-                                          #:max-attempts [max-attempts 3])
-  (-> symbol? (->* ((or/c string? url?))
-                   (#:headers (hash/c symbol? (or/c bytes? string?))
-                    #:params (listof (cons/c symbol? string?))
-                    #:max-attempts exact-positive-integer?)
-                   response?))
-  (define u (if (url? url-or-string) url-or-string (string->url url-or-string)))
-  (define p (url-path-string u))
-  (define s (make-session u))
-  (session-request s p
-                   #:method method
-                   #:headers headers
-                   #:params (append (url-query u) params)
-                   #:max-attempts max-attempts))
+(define (make-requester method)
+  (make-keyword-procedure
+   (lambda (kws kw-args url-or-string . args)
+     (define u (if (url? url-or-string) url-or-string (string->url url-or-string)))
+     (define p (url-path-string u))
+     (define s (make-session u))
+     (keyword-apply session-request kws kw-args s p args #:method method))))
 
 (define-syntax (define-requesters stx)
   (syntax-parse stx
