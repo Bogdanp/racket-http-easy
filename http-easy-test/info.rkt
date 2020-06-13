5a8faf78fc76c7aa11a1c95f5abf8e60a2b30e5d
Bogdan Popa <bogdan@defn.io>
2020-06-13 19:40:46 +0300

    test: add net-cookies-lib build dep

--- http-easy-test/info.rkt
+++ http-easy-test/info.rkt
@@ -3,6 +3,7 @@
 (define collection "tests")
 (define deps '("base"))
 (define build-deps '("http-easy"
+                     "net-cookies-lib"
                      "rackunit-lib"
                      "web-server-lib"))
 (define update-implies '("http-easy"))
