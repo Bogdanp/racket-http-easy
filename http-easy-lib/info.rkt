fb7bb1a0a056fe0a92c2983ed0a09eef987a04c1
Bogdan Popa <bogdan@defn.io>
2025-07-29 16:08:35 +0300

    build: require resource-pool-lib 0.4.1+

--- http-easy-lib/info.rkt
+++ http-easy-lib/info.rkt
@@ -1,10 +1,10 @@
 #lang info
 
 (define license 'BSD-3-Clause)
-(define version "0.9.1")
+(define version "0.9.2")
 (define collection "net")
 (define deps
   '(["base" #:version "8.1.0.4"]
     "net-cookies-lib"
-    ["resource-pool-lib" #:version "0.4"]
+    ["resource-pool-lib" #:version "0.4.1"]
     "unix-socket-lib"))
